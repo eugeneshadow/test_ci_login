de9374e83c323b3e50170711271fd907d4885236
Eugene <eugeneshadow@hotmail.com>
2012-11-30 03:59:46 +0800

    Fix

--- application/views/account_info.php
+++ application/views/account_info.php
@@ -3,7 +3,9 @@
 	<title>Login</title>
 </head>
 <body>
-<?php foreach ($info as $value) {
+<?php
+print_r($info); 
+foreach ($info as $value) {
 	echo "User Name: ". $value['user_id'] . "<br/>"; 	
 	echo "Password: ". $value['password'] . "<br/>"; 	
 	echo "Email: ". $value['email'] . "<br/>"; 	
